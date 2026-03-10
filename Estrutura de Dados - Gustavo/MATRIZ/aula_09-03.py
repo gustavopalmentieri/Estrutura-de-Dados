557882bde5c5f3857451faa1ddeaff9cbacd8925
Gustavo Jesus Palmentieri <gustavoo.palmentieri@gmail.com>
2026-03-09 21:03:06 -0300

    Add files via upload

---
+++ Estrutura de Dados - Gustavo/MATRIZ/aula_09-03.py
@@ -0,0 +1,15 @@
+m = [ #QUALQUER ELEMENTO DE UMA MARIZ TEM DOIS ÍNDICE - LINHA E COLUNA
+
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9], 
+    [2, 4, 6]
+
+    ]
+
+for l in range(4): #RANGE QUE PASSA PELAS LINHAS - QUANTIDADE
+    
+
+    for c in range(3): #RANGE QUE PASSA PELAS COLUNAS - QUANTIDADE
+
+        print(m[l][c], end = )
---
+++ Estrutura de Dados - Gustavo/VETOR/aula_09-03.py
@@ -0,0 +1,11 @@
+v = []
+
+for i in range(7):
+
+    v2 = int(input('Digite um novo valor: '))
+    v.append(v2)
+
+for i in range(7): 
+
+    print(v[i], end=' ') #O 'END' É INTERNO DO PRINT (SERIA O '/N'), AGORA END IGUAL ESPAÇO NÃO PULA LINHA - ESCREVE NA HORIZONTAL COM ESPAÇO.
+
